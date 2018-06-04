3aa0f8bf4bc2642907d8d9a2142f2215faf0e032
wanglezhi <wanglezhi123>
2018-06-04 11:47:24 +0800

    test

--- app/src/main/java/com/example/allin/mytestapplication/MainActivity.java
+++ app/src/main/java/com/example/allin/mytestapplication/MainActivity.java
@@ -18,8 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
     public void goCircle(View view) {
